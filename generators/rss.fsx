263102fc05b910ad9f51c41e4d77622adb2eaa87
Drew Knab <drewknab@gmail.com>
2020-06-07 23:19:24 -0400

    get channel description data from globalloader instead of hard coded, break out parts of channel feed into multiple functions

--- generators/rss.fsx
+++ generators/rss.fsx
@@ -1,45 +1,54 @@
 #r "../_lib/Fornax.Core.dll"
 #if !FORNAX
 #load "../loaders/postloader.fsx"
+#load "../loaders/globalloader.fsx"
 #endif
 
-open System
 open System.Xml.Linq
 
+let xn name = XName.Get name
+let elem name (value : string) = XElement(xn name, value)
 
-let channelFeed
-          (channelTitle : string)
-          (channelLink : string)
-          (channelDescription : string)
-          (items : Postloader.Post list) =
-    let xn name = XName.Get name
-    let elem name (value : string) = XElement(xn name, value)
-    let elems =
-      items
-      |> List.sortBy (fun i -> i.published)
-      |> List.map(fun i ->
-         XElement(xn "item",
-             elem "title" (System.Net.WebUtility.HtmlEncode i.title),
-             elem "link" i.link,
-             elem "guid" i.link,
-             elem "pubDate" (i.published.ToString()),
-             elem "description" (System.Net.WebUtility.HtmlEncode i.summary)
-         ))
+let buildElements (items : Postloader.Post list) =
+    items
+    |> List.sortBy (fun i -> i.published)
+    |> List.map(fun i ->
+        XElement(xn "item",
+            elem "title" (System.Net.WebUtility.HtmlEncode i.title),
+            elem "link" i.link,
+            elem "guid" i.link,
+            elem "pubDate" (i.published.ToString()),
+            elem "description" (System.Net.WebUtility.HtmlEncode i.summary)
+        )
+    )
+
+let channelFeed (channelTitle : string)
+                (channelLink : string)
+                (channelDescription : string)
+                (items : Postloader.Post list) =
+
+    let elems = buildElements items
 
     XDocument(
-      XDeclaration("1.0", "utf-8", "yes"),
-      XElement(xn "rss",
-        XAttribute(xn "version", "2.0"),
-        elem "title" channelTitle,
-        elem "link" channelLink,
-        elem "description" channelDescription,
-        elem "language" "en-us",
-        XElement(xn "channel", elems)
-      ) |> box).ToString()
+        XDeclaration("1.0", "utf-8", "yes"),
+        XElement(xn "rss",
+            XAttribute(xn "version", "2.0"),
+            elem "title" channelTitle,
+            elem "link" channelLink,
+            elem "description" channelDescription,
+            elem "language" "en-us",
+            XElement(xn "channel", elems)
+        ) |> box
+    ).ToString()
 
 
 
 let generate' (ctx : SiteContents) (_ : string) =
+    let (title, link, description) = 
+        ctx.TryGetValue<Globalloader.SiteInfo> ()
+        |> Option.map (fun si -> (si.title, si.url, si.description))
+        |> Option.defaultValue ("", "", "")
+
     let posts =
         ctx.TryGetValues<Postloader.Post>()
         |> Option.defaultValue Seq.empty
@@ -47,7 +56,7 @@ let generate' (ctx : SiteContents) (_ : string) =
 
     [
       "<?xml version='1.0' encoding='UTF-8' ?>"
-      channelFeed "test" "link" "summary" posts
+      channelFeed title link description posts
     ] |> String.concat "\n"
 
 let generate (ctx : SiteContents) (projectRoot : string) (page: string) =
--- loaders/globalloader.fsx
+++ loaders/globalloader.fsx
@@ -3,9 +3,10 @@
 type SiteInfo = {
     title: string
     description: string
+    url: string
 }
 
 let loader (projectRoot: string) (siteContent: SiteContents) =
-    siteContent.Add({title = "Sample Fornax blog"; description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit"})
+    siteContent.Add({title = "Sample Fornax blog"; description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit"; url = "druk.dev"})
 
     siteContent
