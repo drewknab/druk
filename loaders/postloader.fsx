bd9dfc7955f1f2fc1bc34a127b489218058c69ab
Drew Knab <drewknab@gmail.com>
2020-06-08 23:34:57 -0400

    remove live refresh

--- loaders/postloader.fsx
+++ loaders/postloader.fsx
@@ -113,5 +113,5 @@ let loader (projectRoot: string) (siteContent: SiteContents) =
     |> Array.map loadFile
     |> Array.iter siteContent.Add
 
-    siteContent.Add({disableLiveRefresh = false})
+    siteContent.Add({disableLiveRefresh = true})
     siteContent
