2447578d237241f71a5786e1c84934a306e18802
Drew Knab <drewknab@gmail.com>
2021-05-22 00:16:29 -0400

    another attempt

--- build.sh
+++ build.sh
@@ -3,13 +3,7 @@
 # this is needed to get dotnet to run on netlify build image
 export DOTNET_ROOT=/opt/buildhome/.dotnet
 
-wget https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-
-apt-get update; \
-  apt-get install -y apt-transport-https && \
-  apt-get update && \
-  apt-get install -y dotnet-runtime-3.1
+./dotnet-install.sh -c --runtime 3.1.0
 
 dotnet --list-runtimes
 
