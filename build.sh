55358f6c96fe0ab8099cb71bd2aeb792fa56fd66
Drew Knab <drewknab@gmail.com>
2021-05-22 00:09:42 -0400

    attempt

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ export DOTNET_ROOT=/opt/buildhome/.dotnet
 wget https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 
-sudo apt-get update; \
-  sudo apt-get install -y apt-transport-https && \
-  sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-3.1
+apt-get update; \
+  apt-get install -y apt-transport-https && \
+  apt-get update && \
+  apt-get install -y dotnet-runtime-3.1
 
 dotnet --list-runtimes
 
